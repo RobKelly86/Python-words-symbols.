a34a62f13e741dd7e93da1da2e1fd8e2367f91ae
Rob Kelly <98021145+RobKelly86@users.noreply.github.com>
2022-04-11 16:28:46 +0100

    Add files via upload

---
+++ from_example.py
@@ -0,0 +1,14 @@
+# This module is being called upon from symbol.py
+# It is being used as an example for the 'from' import statement.
+
+def fruit1():
+    apple = 1
+    print(f"You have {apple} apple.")
+
+def fruit2():
+    apple = 2
+    print(f"You have {apple} apples.")
+
+def fruit3():
+    apple = 3
+    print(f"You have {apple} apples.")
